6a82ee5bfc888edd845b2e5de73449b6340a13dc
Martin Vaněk <martin.vanek@vrk.cz>
2015-12-06 13:52:49 +0100

    Implementation of ItemMapper

---
+++ organizational-data-layer-implementation/src/main/java/cz/nitramek/organizational/data/implementation/mappers/ItemMapperImpl.java
@@ -0,0 +1,54 @@
+package cz.nitramek.organizational.data.implementation.mappers;
+
+import cz.nitramek.organizational.data.implementation.dto.ItemDTO;
+import cz.nitramek.organizational.data.implementation.util.Converters;
+import cz.nitramek.organizational.data.mapper.ItemMapper;
+import cz.nitramek.organizational.data.util.MapperImplementation;
+import cz.nitramek.organizational.domain.classes.Item;
+import cz.nitramek.organizational.domain.classes.User;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@MapperImplementation(mapper = ItemMapper.class)
+public class ItemMapperImpl implements ItemMapper {
+
+    private final EntityManager em;
+
+    public ItemMapperImpl() throws NamingException {
+        this.em = (EntityManager) new InitialContext().lookup("java:/org-em");
+    }
+
+    @Override
+    public List<Item> select(
+            User user) {
+        return this.em.createNamedQuery("Item.selectByOwner", ItemDTO.class).setParameter("userId", user)
+                      .getResultList().stream()
+                      .map(Converters::convert)
+                      .collect(Collectors.toList());
+    }
+
+    @Override
+    public Item insert(Item item) {
+        ItemDTO iDTO = Converters.convert(item);
+        this.em.persist(iDTO);
+        iDTO.getAttributeDTOs().stream().forEach(this.em::persist);
+
+        return Converters.convert(iDTO);
+    }
+
+    @Override
+    public Item update(Item item) {
+        return this.insert(item);
+    }
+
+    @Override
+    public Item select(long id) {
+        return Converters.convert(this.em.createNamedQuery("Item.selectById", ItemDTO.class)
+                                         .setParameter("id", id)
+                                         .getSingleResult());
+    }
+}
